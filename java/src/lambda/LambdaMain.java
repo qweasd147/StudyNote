9c4394df70c019cee982e0bc61d84a8456072745
KIM\joo <qweasd_147@naver.com>
2018-04-02 14:12:41 +0900

    람다 일부분 완성. 계속 하는중

--- java/src/lambda/LambdaMain.java
+++ java/src/lambda/LambdaMain.java
@@ -1,5 +1,7 @@
 package lambda;
 
+import java.util.function.Function;
+
 public class LambdaMain {
 
     public static void main(String[] args) {
@@ -14,25 +16,47 @@ public class LambdaMain {
         likeCallback(() -> System.out.println("콜백 비슷하게 사용가능!"));
         likeCallback(() -> {
 
-            int num2=5;
-
-
             System.out.println("num : "+num);
             System.out.println("javascript 보단 불편하네요");
             System.out.println("그러긴 하네요");
 
-        });
+            System.out.println("심지어 외부 변수는 final 이예요!");
+            System.out.println("jdk 1.7 이하는 final 선언 안하면 아예 접근도 못해요!");     // ex) final int num = 5;
 
-        likeCallback(new LambdaInterface() {
-            @Override
-            public void doSomeThing() {
-                System.out.println(num);
-            }
+            //num = 5;  ERROR!
         });
     }
 
     public void likeCallback(LambdaInterface cb){
         // 함수를 넘겨 받아 원하는 시점에 호출할수 있다
         cb.doSomeThing();
+    }
+
+    public void closureByJava(){
+        Function<Integer, Integer> func = getFunction();
+
+        Integer result = func.apply(10);
+
+    }
+
+    public static Function getFunction(){
+        int num = 10;
+
+        return (Function<Integer, Integer>) n ->  n*num;
+    }
+
+    public static Function getFunction2(){
+        int num = 100;
+
+        return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer o) {
+
+                //물론 람다 든 아니든 똑같이 오류가 발생한다.
+                //num = 10;     ERROR!
+
+                return null;
+            }
+        };
     }
 }
