b3250f17441e2f22a136c67863a2ebaf464ca59e
김주형 <qweasd_147@naver.com>
2019-10-22 23:07:52 +0900

    필요없는 클래스, import 삭제

--- springboot/BulkOperation/src/main/java/com/example/demo/service/ArticleService.java
+++ springboot/BulkOperation/src/main/java/com/example/demo/service/ArticleService.java
@@ -1,13 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Article;
-import com.example.demo.model.ArticleDto;
 import com.example.demo.model.Tag;
 import com.example.demo.repository.ArticleRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
