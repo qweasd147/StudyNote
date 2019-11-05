d2051ca9f3190e823612a02117a8c4580b76e413
김주형 <qweasd_147@naver.com>
2019-11-05 23:26:20 +0900

    update 쿼리 추가

--- springboot/junit5/src/main/java/com/example/demo/repository/ArticleRepository.java
+++ springboot/junit5/src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,4 +12,9 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     @Query("SELECT article FROM Article article JOIN article.tags tags WHERE tags.tag IN :tags")
     List<Article> findBySeveralTags(@Param("tags") List<String> tags);
+
+    @Modifying
+    @Query("UPDATE Article article SET article.subject = :subject, article.contents = :contents")
+    void updateByQuery(@Param("subject") String subject
+                        , @Param("contents") String name);
 }
