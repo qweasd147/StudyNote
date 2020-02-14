fa4bf274f3420e1a116b25855cc0e683c9bf5cf0
joohyung <qweasd_147@naver.com>
2020-02-14 10:15:12 +0900

    rabbitmq listener 설정 java파일로 변경

--- springboot/rabbitmq/src/main/java/com/example/rabbitmq/config/RabbitmqConfig.java
+++ springboot/rabbitmq/src/main/java/com/example/rabbitmq/config/RabbitmqConfig.java
@@ -1,8 +1,11 @@
 package com.example.rabbitmq.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -58,4 +61,21 @@ public class RabbitmqConfig {
         return retry;
     }
     */
+
+    public SimpleRabbitListenerContainerFactory listenerContainerFactory(ConnectionFactory connectionFactory){
+
+        final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setDefaultRequeueRejected(false);
+        factory.setMessageConverter(messageConverter());
+        factory.setChannelTransacted(true);
+        factory.setAdviceChain(RetryInterceptorBuilder
+                .stateless()
+                .maxAttempts(3)
+                //.recoverer(new RepublishMessageRecoverer())
+                .backOffOptions(1000, 2.0, 10000)
+                .build());
+
+        return factory;
+    }
 }
