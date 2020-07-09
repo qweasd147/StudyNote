d633f4eb4f0a79006bbac96e17a2391099a01bd0
joohyung <qweasd_147@naver.com>
2020-07-09 12:03:37 +0900

    rabbitmq에서 dlx 처리 시 헤더 정보를 확인할 수 있게 수정

--- springboot/rabbitmq/src/main/java/com/example/rabbitmq/consumer/DemoDLXConsumer.java
+++ springboot/rabbitmq/src/main/java/com/example/rabbitmq/consumer/DemoDLXConsumer.java
@@ -2,10 +2,17 @@ package com.example.rabbitmq.consumer;
 
 import com.example.rabbitmq.Item;
 import com.example.rabbitmq.config.RabbitmqConfig;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -13,8 +20,10 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class DemoDLXConsumer {
 
+    private final MessageConverter messageConverter;
 
     @RabbitListener(
         containerFactory = "listenerContainerFactory",
@@ -33,7 +42,19 @@ public class DemoDLXConsumer {
             , key = RabbitmqConfig.DLX_ROUTING_KEY
         )
     )
-    public void failConsumer(Item item){
+    public void failConsumer(Message message){
+
+        Item item = (Item) messageConverter.fromMessage(message);
+        Map<String, Object> header = message.getMessageProperties().getHeaders();
+
+        String exchangeName = (String) header.get("x-first-death-exchange");
+
+        List<Map<String, Object>>xDeath = (List)header.get("x-death");
+        String firstReason = (String) header.get("x-first-death-reason");
+
         log.info("타임아웃된 아이템 수신 {}", item);
+
+        log.warn("reason : {}, exchange name : {}", firstReason, exchangeName);
+        log.warn("x-death : {}", xDeath.get(0).toString());
     }
 }
