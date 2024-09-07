6f26f4f6a5b9f1cb237bc244a514024873e31940
joohyung <qweasd_147@naver.com>
2024-09-08 00:14:36 +0900

    eks 모듈 버전업, 권한은 configmap(aws-auth) 이 아닌 iam으로 eks api로 관리 되도록 변경

--- terraform/eks_module/aws-alb-controller/variables.tf
+++ terraform/eks_module/aws-alb-controller/variables.tf
@@ -9,3 +9,15 @@ variable "vpc_id" {
 variable "oidc_provider_arn" {
   type = string
 }
+
+variable "eks_cluster_endpoint" {
+  type = string
+}
+
+variable "eks_cluster_certificate_authority_data" {
+  type = string
+}
+
+variable "eks_cluster_auth_token" {
+  type = string
+}
--- terraform/eks_module/main.tf
+++ terraform/eks_module/main.tf
@@ -14,24 +14,11 @@ provider "aws" {
   shared_credentials_files = ["~/.aws/credentials_joo"]
 }
 
-provider "kubernetes" {
-  host                   = module.eks.eks-cluster-endpoint
-  cluster_ca_certificate = base64decode(module.eks.eks-cluster-certificate-authority-data)
-  token                  = module.eks.aws-eks-cluster-auth.token
-}
-
-# Terraform에서 helm을 통해 k8s 내 Add-on를 설치할 수 있도록 인증 정보를 제공한다.
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.eks-cluster-endpoint
-    cluster_ca_certificate = base64decode(module.eks.eks-cluster-certificate-authority-data)
-    token                  = module.eks.aws-eks-cluster-auth.token
-  }
-}
-
 locals {
   cluster_name = "test_eks"
 }
+
+data "aws_caller_identity" "current" {}
 
 module "vpc" {
   source       = "./vpc"
@@ -43,11 +30,20 @@ module "eks" {
   cluster_name       = local.cluster_name
   private_subnets_id = module.vpc.private-subnets
   vpc_id             = module.vpc.vpc-id
+  admin_role_account = {
+    arn        = data.aws_caller_identity.current.arn
+    user_id    = data.aws_caller_identity.current.user_id
+    account_id = data.aws_caller_identity.current.account_id
+  }
 }
 
 module "alb" {
-  source            = "./aws-alb-controller"
-  cluster_name      = module.eks.eks-cluster-name
+  source                                 = "./aws-alb-controller"
+  cluster_name                           = module.eks.eks-cluster-name
+  eks_cluster_endpoint                   = module.eks.eks-cluster-endpoint
+  eks_cluster_certificate_authority_data = module.eks.eks-cluster-certificate-authority-data
+  eks_cluster_auth_token                 = module.eks.aws-eks-cluster-auth.token
+
   vpc_id            = module.vpc.vpc-id
   oidc_provider_arn = module.eks.oidc-provider-arn
 }
@@ -73,4 +69,9 @@ output "ecr-repository-url" {
 
 output "ecr-repository-name" {
   value = module.eks.ecr-repository-name
+}
+
+// eks.6
+output "cluster-platform-version" {
+  value = module.eks.eks-cluster-platform-version
 }
